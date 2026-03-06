df62f7ac03c29e402c2f04ff8c8a444576ad631a
mehedyPUST <mehedy.pust@gmail.com>
2026-03-06 22:36:36 +0600

    issue counter designd partially

---
+++ login.js
@@ -0,0 +1,25 @@
+const btnSignIn = document.getElementById('btn-signIn');
+// console.log(btnSignIn);
+
+btnSignIn.addEventListener('click', function () {
+    const userNameInput = document.getElementById('user-name');
+    const userName = userNameInput.value;
+    console.log(userName);
+    const passwordInput = document.getElementById('login-password');
+    const password = passwordInput.value;
+    console.log(password);
+
+    if (userName == 'admin' && password == 'admin123') {
+        // 3.1 true :::> alert > home page 
+        alert('Login Success');
+        // window.location.replace("/home.html");
+        window.location.assign("./main.html");
+
+    } else {
+        // 3.2 false ::: alert > return
+        alert('Login Failed');
+        return;
+    }
+
+
+});
