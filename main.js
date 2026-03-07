e8251e2173c5f18b983b737a3b84d8610bdbd246
mehedyPUST <mehedy.pust@gmail.com>
2026-03-08 01:01:55 +0600

    top border condition added and badges functioned

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-
 const issuesContainer = document.getElementById('issues-container');
+const totalIssuesCounter = document.getElementById('total-issues');
+const allIssuesBtn = document.getElementById('allIssuesBtn');
+const openIssuesBtn = document.getElementById('openIssuesBtn');
+const closedIssuesBtn = document.getElementById('closedIssuesBtn');
+
+let allIssues = [];
 
 async function loadIssues() {
     // showLoading();
@@ -9,95 +14,101 @@ async function loadIssues() {
     // hideLoading();
     console.log(data);
     displayIssues(data.data);
+    totalIssuesCounter.innerText = data.data.length;
 }
-
 
 async function displayIssues(issues) {
     console.log(issues);
+
     issues.forEach((issue) => {
         console.log(issue.labels[1]);
+
         const card = document.createElement('div');
-        card.className = "card  bg-base-100 shadow-sm";
-        card.innerHTML = `<div class="card-body">
-                    <div class="flex justify-between">
-                        <span><img src="${issue.status === 'open' ? './assets/Open-Status.png' : './assets/Closed-Status .png'}" alt="${issue.status} status"></span>
 
+        const borderClass = issue.status === 'open'
+            ? 'border-t-4 border-t-green-500'
+            : 'border-t-4 border-t-purple-500';
 
-                        <span class="py-1 px-4 rounded-xl font-semibold ${issue.priority === 'high' ? 'bg-red-100 text-red-500' :
-                issue.priority === 'medium' ? 'bg-yellow-100 text-yellow-500' :
-                    'bg-gray-100 text-gray-500'}">
-        ${issue.priority === 'high'
+        card.className = `card bg-base-100 shadow-sm ${borderClass}`;
+
+        card.innerHTML = `
+            <div class="card-body">
+                <div class="flex justify-between">
+                    <span>
+                        <img src="${issue.status === 'open' ? './assets/Open-Status.png' : './assets/Closed-Status .png'}" 
+                             alt="${issue.status} status">
+                    </span>
+
+                    <span class="py-1 px-4 rounded-xl font-semibold 
+                          ${issue.priority === 'high'
+                ? 'bg-red-100 text-red-500'
+                : issue.priority === 'medium'
+                    ? 'bg-yellow-100 text-yellow-500'
+                    : 'bg-gray-100 text-gray-500'}">
+                        ${issue.priority === 'high'
                 ? 'HIGH'
                 : issue.priority === 'medium'
                     ? 'MEDIUM'
                     : 'LOW'}
-    </span>
-                    </div >
+                    </span>
+                </div>
 
-                    <h2 class="text-xl font-bold line-clamp-2">${issue.title}</h2>
-                    <p class="line-clamp-3 text-justify">${issue.description}</p>
+                <h2 class="text-xl font-bold line-clamp-2">${issue.title}</h2>
+                <p class="line-clamp-3 text-justify">${issue.description}</p>
 
-                    <div class="flex justify-between">
-
-
-                      <span class="py-1 rounded-xl text-[10px] 
-  ${issue.labels[0] === 'enhancement'
+                <div class="flex gap-3">
+                    <span class="py-1 rounded-xl text-[10px] 
+                          ${issue.labels[0] === 'enhancement'
                 ? 'bg-green-100 px-2 text-green-500 border border-green-500'
                 : issue.labels[0] === 'bug'
-                    ? 'bg-red-100 px-2 text-red-500 border border-red-500'
-                    : 'bg-blue-100 px-2 text-blue-500 border border-blue-500'
-            }"
->
-  ${issue.labels[0] === 'enhancement'
+                    ? 'bg-red-100 px-4 text-red-500 border border-red-500'
+                    : issue.labels[0] === 'good first issue'
+                        ? 'bg-indigo-100 px-2 text-indigo-500 border border-indigo-500'
+                        : 'bg-blue-100 px-2 text-blue-500 border border-blue-500'}">
+                        ${issue.labels[0] === 'enhancement'
                 ? '<i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT'
                 : issue.labels[0] === 'bug'
                     ? '<i class="fa-solid fa-bug"></i> BUG'
-                    : '<i class="fa-regular fa-file-lines"></i> DOCUMENTATION'
-            }
-</span>
+                    : issue.labels[0] === 'good first issue'
+                        ? '<i class="fa-regular fa-star"></i> GOOD FIRST ISSUE'
+                        : '<i class="fa-regular fa-file-lines"></i> DOCUMENTATION'}
+                    </span>
 
+                    <span class="py-1 rounded-xl text-[10px] 
+                          ${!issue.labels[1]
+                ? ''
+                : issue.labels[1] === 'enhancement'
+                    ? 'bg-green-100 px-2 text-green-500 border border-green-500'
+                    : issue.labels[1] === 'good first issue'
+                        ? 'bg-indigo-100 px-2 text-indigo-500 border border-indigo-500'
+                        : issue.labels[1] === 'bug'
+                            ? 'bg-purple-100 px-2 text-purple-500 border border-purple-500'
+                            : issue.labels[1] === 'documentation'
+                                ? 'bg-blue-100 px-2 text-blue-500 border border-blue-500'
+                                : 'bg-yellow-100 px-2 text-yellow-500 border border-yellow-500'}">
+                        ${!issue.labels[1]
+                ? ''
+                : issue.labels[1] === 'enhancement'
+                    ? '<i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT'
+                    : issue.labels[1] === 'good first issue'
+                        ? '<i class="fa-regular fa-star"></i> GOOD FIRST ISSUE'
+                        : issue.labels[1] === 'bug'
+                            ? '<i class="fa-solid fa-bug"></i> BUG'
+                            : issue.labels[1] === 'documentation'
+                                ? '<i class="fa-solid fa-book"></i> DOCUMENTATION'
+                                : '<i class="fa-solid fa-life-ring"></i> HELP WANTED'}
+                    </span>
+                </div>
 
+                <div class="border border-amber-50 shadow-sm py-2">
+                    <p>By: ${issue.author.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}</p>
+                    <p>${new Date(issue.createdAt).toLocaleString()}</p>
+                </div>
+            </div>
+        `;
 
- <span class="py-1 rounded-xl text-[10px] 
-  ${issue.labels[1] === 'enhancement'
-                ? 'bg-green-100 px-2 text-green-500 border border-green-500'
-                : issue.labels[1] === 'bug'
-                    ? 'bg-red-100 px-2 text-red-500 border border-red-500'
-                    : 'bg-blue-100 px-2 text-blue-500 border border-blue-500'
-            }"
->
-  ${issue.labels[1] === 'enhancement'
-                ? '<i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT'
-                : issue.labels[1] === 'bug'
-                    ? '<i class="fa-solid fa-bug"></i> BUG'
-                    : '<i class="fa-regular fa-file-lines"></i> HELP WANTED'
-            }
-</span>
-
-                        
-
-
-                    </div>
-
-
-                    <div class=" border border-amber-50 shadow-sm py-2 ">
-                        <p>#1 by john_doe</p>
-                        <p>1/15/2024</p>
-
-                    </div>
-                </div >
-
-            `;
         issuesContainer.appendChild(card);
-
     });
 }
 
-
-
 loadIssues();
-
-
-// bug icon
-// <i class="fa-solid fa-bug"></i></span>BUG
-// <span class="badge badge-outline badge-warning">${issue.labels[1]}</span>
