4f988678798d56411d9c5c46cc8fb7a0d54b05ae
mehedyPUST <mehedy.pust@gmail.com>
2026-03-07 20:31:59 +0600

    high span 2 design

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ async function loadIssues() {
 async function displayIssues(issues) {
     console.log(issues);
     issues.forEach((issue) => {
-        console.log(issue);
+        console.log(issue.labels[1]);
         const card = document.createElement('div');
         card.className = "card  bg-base-100 shadow-sm";
         card.innerHTML = `<div class="card-body">
@@ -38,10 +38,43 @@ async function displayIssues(issues) {
                     <p class="line-clamp-3 text-justify">${issue.description}</p>
 
                     <div class="flex justify-between">
-                        <span class="badge badge-outline badge-error"><span><i
-                                    class="fa-solid fa-bug"></i></span>BUG</span>
-                        <span class="badge badge-outline badge-warning"><span><i
-                                    class="fa-solid fa-life-ring"></i></span>HELP WANTED</span>
+
+
+                      <span class="py-1 rounded-xl text-[10px] 
+  ${issue.labels[0] === 'enhancement'
+                ? 'bg-green-100 px-2 text-green-500 border border-green-500'
+                : issue.labels[0] === 'bug'
+                    ? 'bg-red-100 px-2 text-red-500 border border-red-500'
+                    : 'bg-blue-100 px-2 text-blue-500 border border-blue-500'
+            }"
+>
+  ${issue.labels[0] === 'enhancement'
+                ? '<i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT'
+                : issue.labels[0] === 'bug'
+                    ? '<i class="fa-solid fa-bug"></i> BUG'
+                    : '<i class="fa-regular fa-file-lines"></i> DOCUMENTATION'
+            }
+</span>
+
+
+
+ <span class="py-1 rounded-xl text-[10px] 
+  ${issue.labels[1] === 'enhancement'
+                ? 'bg-green-100 px-2 text-green-500 border border-green-500'
+                : issue.labels[1] === 'bug'
+                    ? 'bg-red-100 px-2 text-red-500 border border-red-500'
+                    : 'bg-blue-100 px-2 text-blue-500 border border-blue-500'
+            }"
+>
+  ${issue.labels[1] === 'enhancement'
+                ? '<i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT'
+                : issue.labels[1] === 'bug'
+                    ? '<i class="fa-solid fa-bug"></i> BUG'
+                    : '<i class="fa-regular fa-file-lines"></i> HELP WANTED'
+            }
+</span>
+
+                        
 
 
                     </div>
@@ -63,3 +96,8 @@ async function displayIssues(issues) {
 
 
 loadIssues();
+
+
+// bug icon
+// <i class="fa-solid fa-bug"></i></span>BUG
+// <span class="badge badge-outline badge-warning">${issue.labels[1]}</span>
