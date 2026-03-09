b6c9a582d5591220f33b45d79cd8d27ef2dac053
mehedyPUST <mehedy.pust@gmail.com>
2026-03-09 15:15:53 +0600

    almost final

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const openIssuesBtn = document.getElementById('openIssuesBtn');
 const closedIssuesBtn = document.getElementById('closedIssuesBtn');
 const loadingSpinner = document.getElementById('loading-spinner');
 const searchInput = document.getElementById('searchInput');
-
+const modalContent = document.getElementById('modal-content');
+const modalTitle = document.getElementById('modal-title');
+const issueModal = document.getElementById('issue-modal');
 
 let allIssues = [];
 
@@ -20,25 +22,21 @@ function hideLoading() {
     loadingSpinner.classList.add('hidden');
 };
 
-
 async function loadIssues() {
     showLoading();
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
     const res = await fetch(url);
     const data = await res.json();
     hideLoading();
-    // console.log(data);
     allIssues = data.data;
     displayIssues(allIssues);
-    // console.log(data.data);
     totalIssuesCounter.innerText = allIssues.length;
-
 }
 
 async function displayIssues(issues) {
     issuesContainer.innerHTML = '';
+
     if (issues.length === 0) {
-        // Show empty state
         issuesContainer.innerHTML = `
             <div class="col-span-4 text-center py-10 text-gray-500">
                 <i class="fa-regular fa-folder-open text-4xl mb-2"></i>
@@ -48,24 +46,17 @@ async function displayIssues(issues) {
         totalIssuesCounter.innerText = 0;
         return;
     }
-    // console.log(issues);
-
-
-
-
-
-
 
     issues.forEach((issue) => {
-        // console.log(issue.labels[1]);
-
         const card = document.createElement('div');
 
         const borderClass = issue.status === 'open'
             ? 'border-t-4 border-t-green-500'
             : 'border-t-4 border-t-purple-500';
 
-        card.className = `card bg-base-100 shadow-sm ${borderClass}`;
+        card.className = `card bg-base-100 shadow-sm ${borderClass} cursor-pointer hover:shadow-lg transition-shadow`;
+
+        card.addEventListener('click', () => openIssueModal(issue));
 
         card.innerHTML = `
             <div class="card-body">
@@ -76,7 +67,7 @@ async function displayIssues(issues) {
                     </span>
 
                     <span class="py-1 px-4 rounded-xl font-semibold 
-                          ${issue.priority === 'high'
+                        ${issue.priority === 'high'
                 ? 'bg-red-100 text-red-500'
                 : issue.priority === 'medium'
                     ? 'bg-yellow-100 text-yellow-500'
@@ -94,7 +85,7 @@ async function displayIssues(issues) {
 
                 <div class="flex gap-3">
                     <span class="py-1 rounded-xl text-[10px] 
-                          ${issue.labels[0] === 'enhancement'
+                        ${issue.labels[0] === 'enhancement'
                 ? 'bg-green-100 px-2 text-green-500 border border-green-500'
                 : issue.labels[0] === 'bug'
                     ? 'bg-red-100 px-4 text-red-500 border border-red-500'
@@ -111,7 +102,7 @@ async function displayIssues(issues) {
                     </span>
 
                     <span class="py-1 rounded-xl text-[10px] 
-                          ${!issue.labels[1]
+                        ${!issue.labels[1]
                 ? ''
                 : issue.labels[1] === 'enhancement'
                     ? 'bg-green-100 px-2 text-green-500 border border-green-500'
@@ -135,9 +126,11 @@ async function displayIssues(issues) {
                                 : '<i class="fa-solid fa-life-ring"></i> HELP WANTED'}
                     </span>
                 </div>
-
-                <div class="border border-amber-50 shadow-sm py-2">
-                    <p>By: ${issue.author.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}</p>
+<span class="text-gray-300 mt-2 mb-1 -mx-6 ">
+        <hr>
+    </span>
+                <div >
+                    <p>#${issue.id} By: ${issue.author.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}</p>
                     <p>${new Date(issue.createdAt).toLocaleString()}</p>
                 </div>
             </div>
@@ -149,7 +142,110 @@ async function displayIssues(issues) {
     totalIssuesCounter.innerText = issues.length;
 };
 
+function openIssueModal(issue) {
+    const assignee = issue.assignee || "Unassigned";
+    const capitalizedAssignee = assignee.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase());
+    const borderClass = issue.status === 'open'
+        ? 'border-t-4 border-t-green-500'
+        : 'border-t-4 border-t-purple-500';
 
+    modalContent.innerHTML = `
+        <section class="w-[95vw] md:w-[60vw] mx-auto ${borderClass} bg-white shadow-xl p-5 md:p-10 rounded-xl">
+        
+            <div class="flex justify-between items-center mb-3">
+                <h2 class="text-2xl font-bold ">${issue.title}</h2>
+                <label for="issue-modal" class="text-2xl text-red-500 bg-red-100 px-2 rounded-sm cursor-pointer">x</label>
+            </div>
+
+            <div class="flex gap-3 mb-4 items-center ">
+                <span class="badge ${issue.status === 'open' ? 'bg-green-500 ' : ' bg-purple-500'} text-white p-3 rounded-xl">
+                    ${issue.status === 'open' ? 'Opened' : 'Closed'}
+                </span>
+                <span>Opened by: ${issue.author.replace(/_/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}</span>
+                <span>${new Date(issue.createdAt).toLocaleString()}</span>
+            </div>
+
+            <div class="flex gap-3">
+                <span class="py-1 rounded-xl text-[10px] 
+                    ${issue.labels[0] === 'enhancement'
+            ? 'bg-green-100 px-2 text-green-500 border border-green-500'
+            : issue.labels[0] === 'bug'
+                ? 'bg-red-100 px-4 text-red-500 border border-red-500'
+                : issue.labels[0] === 'good first issue'
+                    ? 'bg-indigo-100 px-2 text-indigo-500 border border-indigo-500'
+                    : 'bg-blue-100 px-2 text-blue-500 border border-blue-500'}">
+                    ${issue.labels[0] === 'enhancement'
+            ? '<i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT'
+            : issue.labels[0] === 'bug'
+                ? '<i class="fa-solid fa-bug"></i> BUG'
+                : issue.labels[0] === 'good first issue'
+                    ? '<i class="fa-regular fa-star"></i> GOOD FIRST ISSUE'
+                    : '<i class="fa-regular fa-file-lines"></i> DOCUMENTATION'}
+                </span>
+
+                <span class="py-1 rounded-xl text-[10px] 
+                    ${!issue.labels[1]
+            ? ''
+            : issue.labels[1] === 'enhancement'
+                ? 'bg-green-100 px-2 text-green-500 border border-green-500'
+                : issue.labels[1] === 'good first issue'
+                    ? 'bg-indigo-100 px-2 text-indigo-500 border border-indigo-500'
+                    : issue.labels[1] === 'bug'
+                        ? 'bg-purple-100 px-2 text-purple-500 border border-purple-500'
+                        : issue.labels[1] === 'documentation'
+                            ? 'bg-blue-100 px-2 text-blue-500 border border-blue-500'
+                            : 'bg-yellow-100 px-2 text-yellow-500 border border-yellow-500'}">
+                    ${!issue.labels[1]
+            ? ''
+            : issue.labels[1] === 'enhancement'
+                ? '<i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT'
+                : issue.labels[1] === 'good first issue'
+                    ? '<i class="fa-regular fa-star"></i> GOOD FIRST ISSUE'
+                    : issue.labels[1] === 'bug'
+                        ? '<i class="fa-solid fa-bug"></i> BUG'
+                        : issue.labels[1] === 'documentation'
+                            ? '<i class="fa-solid fa-book"></i> DOCUMENTATION'
+                            : '<i class="fa-solid fa-life-ring"></i> HELP WANTED'}
+                </span>
+            </div>
+
+            <p class="mt-5 text-justify">${issue.description}</p>
+
+            <div class="bg-gray-100 flex gap-20 md:gap-50 mt-5 p-4">
+                <div class="space-y-2">
+                    <p>Assignee:</p>
+                    <p>${capitalizedAssignee}</p>
+                </div>
+                <div class="space-y-2">
+                    <p>Priority:</p>
+                    <span class="py-1 px-4 rounded-xl font-semibold 
+                        ${issue.priority === 'high'
+            ? 'bg-red-100 text-red-500'
+            : issue.priority === 'medium'
+                ? 'bg-yellow-100 text-yellow-500'
+                : 'bg-gray-100 text-gray-500'}">
+                        ${issue.priority === 'high'
+            ? 'HIGH'
+            : issue.priority === 'medium'
+                ? 'MEDIUM'
+                : 'LOW'}
+                    </span>
+                </div>
+            </div>
+
+            <div class="flex justify-end mt-5">
+                <button onclick="closeModal()" class="btn btn-primary">Close</button>
+            </div>
+
+        </section>
+    `;
+
+    issueModal.checked = true;
+}
+
+function closeModal() {
+    issueModal.checked = false;
+}
 
 function filterAllIssues() {
     showLoading();
@@ -170,9 +266,8 @@ function filterOpenIssues() {
     allIssuesBtn.className = 'btn btn-outline col-span-2 md:col-span-1 ';
     openIssuesBtn.className = 'btn bg-green-500 text-white ';
     closedIssuesBtn.className = 'btn btn-outline ';
-
-
 }
+
 function filterClosedIssues() {
     showLoading();
     const closedIssues = allIssues.filter(issue => issue.status === 'closed');
@@ -181,9 +276,7 @@ function filterClosedIssues() {
 
     allIssuesBtn.className = 'btn btn-outline col-span-2 md:col-span-1 ';
     openIssuesBtn.className = 'btn btn-outline ';
-    closedIssuesBtn.className = 'btn  bg-purple-500 text-white ';
-
-
+    closedIssuesBtn.className = 'btn bg-purple-500 text-white ';
 }
 
 allIssuesBtn.addEventListener('click', filterAllIssues);
@@ -196,21 +289,17 @@ async function search(event) {
     const searchTerm = event.target.value;
 
     if (searchTerm === '') {
-        loadIssues();
-        hideLoading();
+        await loadIssues();
     } else {
         const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchTerm}`;
 
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                const searchResults = data.data;
-                displayIssues(searchResults);
-            });
+        const res = await fetch(url);
+        const data = await res.json();
+        const searchResults = data.data;
+        await displayIssues(searchResults);
     }
-
-};
+    hideLoading();
+}
 
 
 loadIssues();
-
